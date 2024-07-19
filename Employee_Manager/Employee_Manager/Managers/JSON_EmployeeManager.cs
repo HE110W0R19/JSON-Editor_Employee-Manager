f4c1f5b304b9edc9d0f418c244d226c60f62297a
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2024-07-20 00:33:39 +0300

    RU/// -> Добавил проект
    -> Добавил файлик Models, где хранятся основные сущности(классы) для работы с ними
    
    -> Добавил файлик Managers, в котором хранятся классы для работы с конкретным форматом файла(в будущем можно будет добавить класс для работы с XML файликами)
    
    p.s Добавил начальные классы Employe и JSON-Manager

---
+++ Employee_Manager/Employee_Manager/Managers/JSON_EmployeeManager.cs
@@ -0,0 +1,20 @@
+﻿using Employee_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Manager.Managers
+{
+    public class JSON_EmployeeManager
+    {
+        private readonly string? _filePath;
+        private List<Employee>? _employees;
+
+        public JSON_EmployeeManager(string filePath)
+        {
+            _filePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
+        }
+    }
+}
---
+++ Employee_Manager/Employee_Manager/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("ERROR: No argumenst for manager!");
+            return;
+        }
+    }
+}
